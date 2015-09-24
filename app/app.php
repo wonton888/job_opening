330d1ad3ad6ff93097c82b62f5ced0a7a72df298
Adam Won <wonton916@gmail.com>
2015-09-24 13:12:23 -0700

    add app.php to job_posting

--- app/app.php
+++ app/app.php
@@ -2,7 +2,7 @@
     require_once __DIR__ ."/../vendor/autoload.php";
     require_once __DIR__ ."/../src/job.php";
 
-    session start();
+    session_start();
 
     if (empty($_SESSION["list_of_jobs"])){
         $SESSION["list_of_jobs"] = array();
@@ -20,7 +20,7 @@
     });
 
     $app->post("/jobs", function() use ($app){
-        $job = new JobOpening($_POST['name'], $_POST['title'], $_POST['job_description'], $_POST['contact_email'], $_POST['contact_phone'])
+        $job = new JobOpening($_POST['name'], $_POST['title'], $_POST['job_description'], $_POST['contact_email'], $_POST['contact_phone']);
         $job->save();
         return $app['twig']->
         render('jobs.html.twig', array('jobs' => JobOpening::getAll()));
